fd2408801956a224d98fe605a0b999acd7695624
Arnav Gudibande <arnav.gudibande@gmail.com>
2016-09-23 21:33:39 -0700

    init commit

--- Encryption-Challenge/ViewController.swift
+++ Encryption-Challenge/ViewController.swift
@@ -1,25 +1,62 @@
 //
 //  ViewController.swift
-//  Encryption-Challenge
+//  RedisDemo
 //
-//  Created by Arnav Gudibande on 9/23/16.
+//  Created by Arnav Gudibande on 9/14/16.
 //  Copyright © 2016 Arnav Gudibande. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+extension String {
+    
+    subscript (i: Int) -> Character {
+        return self[self.index(startIndex, offsetBy: i)]
+    }
+    
+    subscript (i: Int) -> String {
+        return String(self[i] as Character)
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let start = self.index(startIndex, offsetBy: r.lowerBound)
+        let end = self.index(startIndex, offsetBy: (r.upperBound-r.lowerBound))
+        return self[Range(start ..< end)]
+    }
+}
 
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var decryptedWord = "" // This is where you will store your answer to
+        
+        let name = "Amar_R" // Your first name here!
+        
+        // Solve the problem
+        
+        let cipher = "qwertyuiopasdfghiklzxcvbnm"
+        var encrypted = "ztlzofu"
+        let alphabet = "abcdefghijklmnopqrstuvwxyz"
+        
+        
+        for (_, element1) in encrypted.characters.enumerated() {
+            for(index, element) in cipher.characters.enumerated() {
+                if(element1==element) {
+                    decryptedWord += alphabet[index]
+                }
+            }
+        }
+        
+        print(decryptedWord)
+        
+        
+        decryptedWord = "" // your final answer!
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
